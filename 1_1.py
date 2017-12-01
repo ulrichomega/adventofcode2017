426cb465a2110787ffdccbb9d230c0a5565fea99
Daniel Newton <ulrichomega@Daniels-MBP.home>
2017-12-01 17:35:31 -0500

    Answer to day 1

--- 1_1.py
+++ 1_1.py
@@ -1,5 +1,20 @@
+import sys
 
 def main():
-    input = open("input_1_1.txt")
+    with open(sys.argv[1]) as input:
+        contents = input.read().strip()
+    print(contents)
+    total=0
 
-    contents = 
+    for x in range(-1,len(contents)-2):
+        first = contents[x]
+        second = contents[x+1]
+        #print ("comparing {first} and {second}".format(first=first,second=second))
+        if first == second:
+            total = total + int(first)
+            #print ("match: Total is: {total}".format(total=total))
+
+    print(total)
+
+if __name__ == "__main__":
+    main()
