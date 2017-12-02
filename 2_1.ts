e28b8aaf2bd11da50c5a651eb50325868cb92593
Daniel Newton <ulrichomega@Daniels-MacBook-Pro.local>
2017-12-02 10:27:44 -0500

    Working version of 2_1

--- 2_1.ts
+++ 2_1.ts
@@ -1,3 +1,17 @@
 import fs = require("fs");
+import _ = require("lodash");
 
-fs.readFileSync("./input_2_1.txt");
+const fileContents: string = fs.readFileSync("./input_2_1.txt", {encoding: "utf8"}).trim();
+
+const stringResults: string[][] = _.map(_.split(fileContents, "\n"), (input: string) => {
+    return _.split(input, "\t");
+});
+console.log(stringResults);
+
+const intResults: number[][] = _.map(stringResults, (input: string[]) => {
+    return _.map(input, _.parseInt);
+});
+
+console.log(_.sum(_.map(intResults, (input: number[]) => {
+    return Math.abs(_.min(input) - _.max(input));
+})));
