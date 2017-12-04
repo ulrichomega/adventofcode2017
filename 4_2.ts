9f367d57f9a05e256ae65f401cef8f65fcd6d677
Daniel Newton <ulrichomega@Daniels-MBP.home>
2017-12-04 16:47:56 -0500

    Solution to 4

---
+++ 4_2.ts
@@ -0,0 +1,24 @@
+import fs = require("fs");
+import _ = require("lodash");
+
+const fileContents: string = fs.readFileSync("./input_4_1.txt", {encoding: "utf8"}).trim();
+
+const words: string[][] = _.map(_.split(fileContents, "\n"), (input: string) => {
+    return _.split(input, " ");
+});
+
+const output: number = _.sum(_.map(words, (line: string[]): number => {
+    // Sort every word, and then compare them
+    // Two words that sort into the same string are anagrams, and thus not valid
+    // Since they're the same, they'll also disappear from uniq
+    const originalCounts: string[] = _.map(line, (word: string): string => {
+        // Surely there's a way to sort strings, but javascript doesn't need one
+        // And typescript doesn't seem to have one. So...
+        return _.toString(_.toArray(word).sort());
+    });
+    const valid: boolean = _.isEqual(originalCounts, _.uniq(originalCounts));
+
+    return valid ? 1 : 0;
+}));
+
+console.log(output);
